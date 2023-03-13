449e2f77d0620bd0d50ce5a8011487142e3a40fd
randysongEXE <127687854+randysongEXE@users.noreply.github.com>
2023-03-12 23:02:38 -0500

    Create Fibonacci48.java
    The Fibonacci sequence is attributed to Italian mathematician Leonardo of Pisa. It is represented as a sequence of integers, starting with 0, 1 where every number after the first 2 is the sum of the two preceding numbers. The beginning of the Fibonacci sequence looks like...
    
    0, 1, 1, 2, 3, 5, 8, 13, 21, 34
    
    Your task is to write a program which renders the nth number of the Fibonacci sequence. For example if given a value of 9, you are to return the 9th number of the sequence, which would be the number 21.
    
    Input Format
    
    The first line will contain a positive integer  denoting the number of test cases that follow. Each test case will have the following input:
    
    - A single positive integer which represents the position in the sequence for which you are to determine the value.
    Constraints
    
    
    Output Format
    
    - For each test case, your program should output one line containing the sequence position and the value of the integer at that position in the Fibonacci sequence. Each line of output should be in the format:
    
    POSITION = VALUE
    
    Sample Input 0
    
    7
    1
    5
    8
    11
    13
    21
    40
    
    
    Sample Output 0
    
    1 = 0
    5 = 3
    8 = 13
    11 = 55
    13 = 144
    21 = 6765
    40 = 63245986

---
+++ Fibonacci48.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        scan.nextLine();
+        
+        while(T-->0){
+            int n = scan.nextInt();
+            System.out.println(n + " = " + findIndex(n-1));
+        }
+        
+        }
+
+    public static int findIndex(int n){
+        
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else
+            return findIndex(n-1) + findIndex(n-2);
+        
+
+    }
