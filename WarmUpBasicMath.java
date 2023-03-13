a6e58ce9aa4ffa3faa7c383322d24adac9fc1fdd
randysongEXE <127687854+randysongEXE@users.noreply.github.com>
2023-03-12 22:59:40 -0500

    Create WarmUpBasicMath.java
    Given a integers N performs the following: - print all number starting at 1 ending at N with a space between each number - check if N is greater than 5 but less than or equal to 10 - multiply N * pi and round the answer to 6 decimal places - check if N is a prime number - find the least common multiple of N and 4
    
    Input Format
    
    N
    
    Constraints
    
    N is an integer greater than 1
    
    Output Format
    
    -The first line is the list of numbers from 1 to N
    -The second line is "Yes" if N is greater than 5 but less than or equal to 10, otherwise print "No"
    -The third line is N * pi rounded to 6 decimal places
    -The fourth line is the least common multiple of N and 4
    
    Sample Input 0
    
    6
    
    Sample Output 0
    
    1 2 3 4 5 6 
    Yes
    18.849556
    12

---
+++ WarmUpBasicMath.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math.*;
+import java.text.DecimalFormat;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+     
+        
+        Scanner key = new Scanner(System.in);
+        
+        System.out.print("");
+        int input = key.nextInt();
+        
+        ListNums(input);
+        System.out.println();
+        YesOrNo(input);
+        piMulti(input);
+        lcmMethod(input,4);
+    }
+    
+    
+    public static void ListNums(int num){
+        for(int i = 1; i <= num; i++){
+            System.out.print(i + " ");
+        }
+    }
+    
+    public static void YesOrNo(int num){
+        if(num > 5 && num <=10){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+    
+    public static void piMulti(int num){
+        double pi = Math.PI;
+        double Placeholder = num * pi;
+        
+        DecimalFormat obj = new DecimalFormat("#.######");
+        System.out.println(obj.format(Placeholder));
+    }
+    
+    public static void lcmMethod(int n1, int n2){
+        
+        int lcm;
+        
+        lcm = (n1>n2) ? n1 : n2;
+        
+        while(true){
+            if(lcm%n1 == 0 & lcm % n2 == 0){
+                System.out.print(lcm);
+                break;
+            }
+            ++lcm;
+        }
+                
+        
+    }
+}
